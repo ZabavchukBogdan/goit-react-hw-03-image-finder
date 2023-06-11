93ad0c4461b474399c4d410de294a8afbc145e22
ZabavchukBogdan <117076795+ZabavchukBogdan@users.noreply.github.com>
2023-06-11 17:13:27 +0300

    fix 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,7 @@ export class App extends Component {
     error: false,
   };
 
-  // Стадія оновленння (життєвий цикл)
+  // метод отримання та збереження перших зображень у state
   componentDidUpdate(_, prevState) {
     const { page, textForSearch, per_page } = this.state;
 
@@ -35,7 +35,7 @@ export class App extends Component {
     ) {
       this.setState({ status: 'pending' });
 
-      //Fetch
+      
       getPictures(textForSearch, page, per_page)
         .then(elements => {
           if (!elements.hits.length) {
@@ -56,7 +56,7 @@ export class App extends Component {
     }
   }
 
-  //Приймаємо та оновлюємо данні в this.state
+  //додаємо данні в this.state
   handleFormSubmit = textForSearch => {
     this.setState({ textForSearch, page: 1, pictures: [] });
   };
@@ -121,126 +121,6 @@ export class App extends Component {
     );
   }
 }
-
-
-
-// export class App extends Component {
-//    state = {
-//     textForSearch: '',
-//     page: 1,
-//     pictures: [],
-//     totalPages: 0,
-//     per_page: 12,
-//     status: 'idle',
-//     modalImgURL: '',
-//     tagsImg: '',
-//     showModal: false,
-//     error: false,
-//   };
-
-
-
-//   // метод отримання та збереження перших зображень у state
-//   async componentDidUpdate(_, prevState) {
-//      const { page, textForSearch, per_page } = this.state;
-
-//     if (
-//       prevState.page !== this.state.page ||
-//       prevState.textForSearch !== this.state.textForSearch
-//     ) {
-//       this.setState({ status: 'pending' });
-
-//      getPictures(textForSearch, page, per_page)
-//         .then(elements => {
-//           if (!elements.hits.length) {
-//             this.setState({ status: 'idle' });
-//             return  toast.info(`Зображення ${textForSearch} відсутні`);
-//           }
-
-//           this.setState(prevState => ({
-//             pictures: [...prevState.pictures, ...elements.hits],
-//             totalPages: Math.ceil(elements.totalHits / per_page),
-//             status: 'idle',
-//           }));
-//         })
-//         .catch(error => {
-//           this.setState({ error: true });
-//           console.log(error);
-//         });
-//     }
-//   }
-
-//   // метод отримання та збереження у state данних
-//   receiveTextForSearch = textForSearch => {
-//      this.setState({ textForSearch, page: 1, pictures: [] });
-//   };
-
-
-
-//   // додавання сторінки
-
-//   handleLoadMore = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//   };
-
-
-
-//   // метод відкриття модалки
-//    getImgData = (modalImgURL, tagsImg) => {
-//     this.setState({ modalImgURL: modalImgURL, tagsImg: tagsImg });
-//     this.toggleModal();
-//    };
-  
-//   // метод закриття модалки
-//    toggleModal = () => {
-//     this.setState(state => ({
-//       showModal: !state.showModal,
-//     }));
-//    };
-  
-//     render() {
-//     const {
-//       status,
-//       modalImgURL,
-//       tagsImg,
-//       showModal,
-//       page,
-//       totalPages,
-//       pictures,
-//       error,
-//     } = this.state;
-
-//     return (
-//       <Wrapper>
-//         <Searchbar onSubmit={this.receiveTextForSearch} />
-//         {status === 'pending' && !error && <Loader />}
-//         {pictures.length > 0 && !error && (
-//           <ImageGallery pictures={pictures} onClick={this.getImgData} />
-//         )}
-
-//         {pictures.length > 0 && totalPages !== page && !error && (
-//           <Button onClick={this.handleLoadMore} />
-//         )}
-
-//         {showModal && (
-//           <ModalImg
-//             modalImgURL={modalImgURL}
-//             tagsImg={tagsImg}
-//             onClose={this.toggleModal}
-//           />
-//         )}
-
-
-//          <ToastContainer autoClose={3000} />
-//       </Wrapper>
-//     );
-//   }
-  
- 
-// }
-
 
 
 
