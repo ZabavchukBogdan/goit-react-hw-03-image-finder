13bab24ff3dbf2ad9709c59d6213faa9bf877b71
ZabavchukBogdan <117076795+ZabavchukBogdan@users.noreply.github.com>
2023-06-11 18:54:07 +0300

    fix bild 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Component } from 'react';
 import { Notify } from 'notiflix';
 import { getPictures } from 'services/api';
-import { SearchBar } from './Search/Searchbar'
-import { ImageGallery } from './ImageGallery/ImageGallery';
-import { Button } from './Button/Button'
-import { Loader } from './Loader/Loader';
-import { Modal } from './Modal/Modal';
+import { SearchBar } from './search/Searchbar'
+import { ImageGallery } from './imageGallery/ImageGallery';
+import { Button } from './button/Button'
+import { Loader } from './loader/Loader';
+import { Modal } from './modal/Modal';
 import { AppContainer} from './App.styled';
 
 export class App extends Component {
